13327f4de612b9e2cf9eefac3976048dd69de36b
Akshay Tomar <75598614+Akshay-Tomar-1135@users.noreply.github.com>
2024-11-13 17:36:31 +0530

    Userdashboard (#19)
    * Added location panel
    
    * Made minor changes in section/user
    
    * Created Profile Page
    
    * Implemented apis in userService
    
    * PR comments resolved
    
    ---------
    
    Co-authored-by: adityaS-99 <00007adi@gmail.com>
    Co-authored-by: Akshay-Tomar-1135 <akshaytomar1135@gmai.com>

--- src/sections/user/confirm-delete-dialog.tsx
+++ src/sections/user/confirm-delete-dialog.tsx
@@ -1,31 +1,25 @@
-import {
-    Dialog,
-    DialogActions,
-    DialogContent,
-    DialogTitle,
-    Button,
-  } from '@mui/material';
-  
-  type ConfirmDeleteDialogProps = {
-    open: boolean;
-    onClose: () => void;
-    onConfirm: () => void;
-    userName?: string; 
-  };
-  
-  export function ConfirmDeleteDialog({
-    open,
-    onClose,
-    onConfirm,
-    userName,
-  }: ConfirmDeleteDialogProps) {
-    return (
-      <>
+import { Dialog, DialogActions, DialogContent, DialogTitle, Button } from '@mui/material';
+
+type ConfirmDeleteDialogProps = {
+  open: boolean;
+  onClose: () => void;
+  onConfirm: () => void;
+  userName?: string;
+};
+
+export function ConfirmDeleteDialog({
+  open,
+  onClose,
+  onConfirm,
+  userName,
+}: ConfirmDeleteDialogProps) {
+  return (
+    <>
       <Dialog open={open} onClose={onClose}>
         <DialogTitle>Confirm Deletion</DialogTitle>
         <DialogContent>
-        Are you sure you want to delete{' '}
-        {userName ? <strong>&quot;{userName}&quot;</strong> : 'this user'}?
+          Are you sure you want to delete{' '}
+          {userName ? <strong>&quot;{userName}&quot;</strong> : 'this user'}?
         </DialogContent>
         <DialogActions>
           <Button onClick={onClose}>Cancel</Button>
@@ -34,7 +28,6 @@ import {
           </Button>
         </DialogActions>
       </Dialog>
-      </>
-    );
-  }
-  
+    </>
+  );
+}
